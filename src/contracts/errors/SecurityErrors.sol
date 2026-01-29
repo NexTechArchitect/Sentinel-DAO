b94ba585d2e97ca9202492c64606b4ee06751421
PixelSyntax <amitthapa181133@gmail.com>
2026-01-29 22:06:30 +0530

    Update SecurityErrors.sol

--- src/contracts/errors/SecurityErrors.sol
+++ src/contracts/errors/SecurityErrors.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.30;
 
+//////////////////////////////////////////
+//             SecurityErrors           //
+//////////////////////////////////////////
+
 error OnlyAdmin();
 error OnlyGuardian();
 error RoleAlreadyGranted();
