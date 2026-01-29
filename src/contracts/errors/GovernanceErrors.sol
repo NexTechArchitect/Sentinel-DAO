17835fa360e9c0de2997a184d3ac6e2d65142f93
PixelSyntax <amitthapa181133@gmail.com>
2026-01-29 22:04:26 +0530

    Update GovernanceErrors.sol

--- src/contracts/errors/GovernanceErrors.sol
+++ src/contracts/errors/GovernanceErrors.sol
@@ -1,36 +1,38 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.30;
 
+////////////////////////////////////////////////
+//              GovernorErrors                //
+////////////////////////////////////////////////
+
 error OnlyTimelock();
 error ModuleAlreadyExists();
 error ModuleNotFound();
 error SameAddress();
 error AlreadyLocked();
-// --- Governor & Voting ---
+
+
 error InvalidProposalId();
 error ProposalNotActive();
 error VotingNotStarted();
 error VotingEnded();
-error InvalidSupport(); // For vote type (0,1,2)
+error InvalidSupport(); 
 error AlreadyVoted();
 
-// --- Governance Token ---
 error NotGovernance();
 error MaxSupplyExceeded();
 
-// --- Strategies & Snapshots ---
 error InvalidStrategy();
 error SameStrategy();
 error SnapshotAlreadyExists(uint256 proposalId);
 error SnapshotNotFound(uint256 proposalId);
 
-// --- Offchain Execution ---
 error ResultAlreadyExecuted();
 error InvalidSignature();
 error SignatureExpired();
 error Unauthorized();
   
-  // --- Timelock ---
+
  error InvalidMinDelay(uint256 provided, uint256 min, uint256 max);
     error EmptyProposers();
     error EmptyExecutors();
